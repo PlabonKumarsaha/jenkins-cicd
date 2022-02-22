d456e7f01d27da66bf0d50bb208810210600b7a1
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-02-22 10:33:54 +0600

    deployable

--- src/main/java/com/pks/cicd/CicdApplication.java
+++ src/main/java/com/pks/cicd/CicdApplication.java
@@ -2,9 +2,19 @@ package com.pks.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdApplication {
+public class CicdApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+	{
+		return builder.sources(CicdApplication.class);
+	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdApplication.class, args);
